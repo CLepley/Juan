9ec1e0a28e1bfe74540e1ef6e8ed91dc75f811ce
chris Lepley <chrislepley@chriss-MacBook-Pro-2.local>
2015-02-26 21:46:38 -0500

    re-added cannon jpg

---
+++ Classes/BuildingObject.h
@@ -0,0 +1,14 @@
+//
+//  BuildingObject.h
+//  Juan
+//
+//  Created by chris Lepley on 2/26/15.
+//
+//
+
+#ifndef __Juan__BuildingObject__
+#define __Juan__BuildingObject__
+
+#include <stdio.h>
+
+#endif /* defined(__Juan__BuildingObject__) */
